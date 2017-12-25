b94ef47c8e33046a54de9e3a4981df9da8c7c7ac
Nathan Schneider <nathan.schneider@georgetown.edu>
2017-12-25 23:02:49 +0200

    script to generate .conllulex file, and supporting script to determine LexCat

---
+++ lexcatter.py
@@ -0,0 +1,93 @@
+
+def compute_lexcat(tokNum, smwe, smweGroupToks, ss, lexlemma, poses, rels):
+    """
+    The lexical category, or LexCat, is the syntactic category of a strong
+    lexical expression, which may be a single-word or multiword expression.
+    The value of LexCat is determined in part from the UPOS (Universal POS)
+    and XPOS (for English, PTB tag), as follows:
+
+    1. If the expression has been annotated as ... the LexCat is ...
+        `c    CCONJ
+        `j    ADJ
+        `r    ADV
+        `o    PRON
+        `d    DISC
+        `i    INF
+        `a    AUX       (note: the UPOS should usually be AUX)
+    2. If the expression has been annotated with a ... supersense, the LexCat is ...
+        noun    N
+        verb    V
+    3. If the expression has been annotated with an adposition supersense or `$:
+       a. If XPOS is ... the LexCat is ...
+           PRP$     PRON.POSS
+           WP$      PRON.POSS
+           POS      POSS
+           TO       INF.P
+          (all `$ tokens should be matched by one of these conditions)
+       b. If a multiword expression:
+           i. If the last token of the MWE has UPOS of `ADP` or `SCONJ`, LexCat is `P`.
+           ii. Otherwise, LexCat is `PP`.
+       c. Otherwise, LexCat is `P`.
+    4. Other tokens with UPOS of `NOUN`, `VERB`, `ADP`, or XPOS of `POS`, `PRP$`, `WP$`, `TO`: need examination. [N.B. `PART` = `TO` + `POS` + negative markers]
+    5. Otherwise, if the UPOS is `PART`, LexCat is `ADV` (negative markers).
+    6. Other strong MWEs need examination. Some special cases are handled.
+    7. Otherwise, the LexCat is the same as the UPOS.
+
+    The script that performs automatic assignment should have an option to suffix any default (non-human) annotations with `@` and any problematic cases with `!`.
+    """
+    if smwe!='_' and not smwe.endswith(':1'):
+        # non-initial token in MWE
+        return '_'
+
+    lc = {'`a': 'AUX', '`c': 'CCONJ', '`d': 'DISC', '`i': 'INF', '`j': 'ADJ',
+          '`o': 'PRON', '`r': 'ADV', '??': '??'}.get(ss)
+
+    if lc is not None: return lc
+    if ss.isalpha() and ss.isupper(): return 'N'
+    if ss.isalpha() and ss.islower(): return 'V'
+
+    upos, xpos = poses[tokNum-1]
+    if ss=='`$' or (ss[0].isupper() and ss[1].islower()):
+        lc = {'PRP$': 'PRON.POSS', 'WP$': 'PRON.POSS', 'POS': 'POSS', 'TO': 'INF.P'}.get(xpos)
+        if lc is not None: return lc
+        assert ss!='`$'
+        if smwe!='_':
+            if poses[smweGroupToks[-1]-1][0] in ('ADP','SCONJ'):
+                return 'P'
+            return 'PP'
+        return 'P'
+    if upos in ('NOUN', 'VERB', 'ADP') or xpos in ('POS', 'PRP$', 'WP$', 'TO'):
+        return '!!@'
+    if upos=='PART':
+        return 'ADV'
+
+    if smwe!='_':
+        if upos=='DET':
+            if lexlemma in ('a lot', 'a couple', 'a few', 'a little', 'a bit', 'a number', 'a bunch'):
+                return 'DET'
+            elif lexlemma in ('no one', 'every one', 'every thing', 'each other', 'some place'):
+                return 'PRON'
+
+        head,rel = rels[tokNum-1]
+        if head in smweGroupToks:
+            return compute_lexcat(head, '_', smweGroupToks, ss, lexlemma, poses, rels)
+            """if upos=='ADJ' and lc=='!!':
+                # ADJ-NOUN compounds functioning as adjectives: the following occur in Reviews
+                if lexlemma in ('first - class', 'first class', 'first - time', 'first time',
+                    'full - time', 'full time', 'high - dollar', 'high dollar',
+                    'high - end', 'high end', 'high - tech', 'high tech',
+                    'old - time', 'old time', 'top - notch', 'top notch'):
+                    return 'ADJ'
+
+                if lexlemma in ('bottom line', 'parisian croissant', 'straight edge'):
+                    return 'NOUN'
+
+                if lexlamma in ('happy new year', 'good luck', 'good job',
+                    'sad face', 'holy cow'):
+                    return 'DISC'
+                """
+        else:
+            assert upos!='X'    # X is used for goeswith (also 'sub <advmod par').
+            # In those cases the head should be in the MWE.
+        return '!@'
+    return upos
---
+++ sst2conllulex.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+"""
+Given input in the .sst format, converts to the .conllulex format
+(an extension of the tabular .conllu format with additional columns
+for lexical semantics).
+
+A path to the reviews subdirectory of the UD_English repository
+is specified in a global variable.
+"""
+
+REVIEWSDIR = 'UD_English/not-to-release/sources/reviews'
+
+import fileinput, json, re, sys
+
+from lexcatter import compute_lexcat
+
+sentSSTData = []
+docids = set()
+
+for ln in fileinput.input():
+    if ln.strip():
+        sentid, mweMarkup, data = ln.strip().split('\t')
+        data = json.loads(data)
+        # STREUSLE sentence ID: ewtb.r.093655.7
+        corpid, subcorpid, docid, sentnum = sentid.split('.')
+        assert (corpid,subcorpid)==('ewtb','r'),sentid # reviews
+        # infer UD sentence ID: reviews-093655-0007
+        udsentid = f'reviews-{docid}-{int(sentnum):04}'
+        sentSSTData.append((sentid, udsentid, mweMarkup, data))
+        docids.add(docid)
+
+ud = {}
+
+for docid in docids:
+    udFP = f'{REVIEWSDIR}/{docid}.xml.conllu'
+    with open(udFP) as udF:
+        udsentid = None
+        sentMetaLines = []
+        sentTokLines = []
+        lemmas = []
+        poses = []
+        rels = []
+        for ln in udF:
+            if not ln.strip():
+                assert udsentid is not None
+                ud[udsentid] = (sentMetaLines, sentTokLines, lemmas, poses, rels)
+                sentid = None
+                sentMetaLines = []
+                sentTokLines = []
+                lemmas = []
+                poses = []
+                rels = []
+                continue
+            ln = ln.strip()
+
+            if ln.startswith('#'):
+                if ln.startswith('# sent_id = '):
+                    udsentid = ln.split()[-1]
+                    subcorpid, docid_, sentnum = udsentid.split('-')
+                    assert docid_==docid
+                    assert subcorpid=='reviews'
+                sentMetaLines.append(ln)
+            else:
+                assert ln.count('\t')==9,ln
+                if re.match(r'^\d+\t', ln):
+                    lemmas.append(ln.split('\t')[2])
+                    poses.append(ln.split('\t')[3:5])
+                    rels.append(ln.split('\t')[6:8])
+                    rels[-1][0] = int(rels[-1][0])
+                sentTokLines.append(ln)
+
+N_NEW_COLS = 9
+
+for sentid, udsentid, mweMarkup, data in sentSSTData:
+    data["wmwecat"] = []   # no weak MWE syntactic categories in this release
+    udMetaLines, udTokLines, udLemmas, poses, rels = ud[udsentid]
+    for ln in udMetaLines:
+        print(ln)
+    print(f'# streusle_sent_id = {sentid}')
+    print(f'# mwe = {mweMarkup}')
+    smweGroup = {}
+    smweGroupToks = {}
+    lexLemmas = {}
+    wmweGroup = {}
+    wLemmas = {}
+    i = 1
+    for i,sg in enumerate(data["_"], 1):
+        for j,o in enumerate(sg, 1):
+            assert o not in smweGroup
+            smweGroup[o] = f'{i}:{j}'
+            smweGroupToks[o] = sg
+        lexLemmas[sg[0]] = ' '.join(udLemmas[j-1] for j in sg)
+    for h,wg in enumerate(data["_"], i+1):
+        for j,o in enumerate(wg, 1):
+            assert o not in wmweGroup
+            wmweGroup[o] = f'{h}:{j}'
+        wLemmas[wg[0]] = ' '.join(udLemmas[j-1] for j in wg)
+    for ln in udTokLines:
+        tokNum, form, lemma, upos, xpos, feats, head, deprel, deps, misc = ln.split('\t')
+        if re.match(r'^\d+$', tokNum):
+            tokNum = int(tokNum)
+            offset0 = tokNum-1
+            if data["words"][offset0] != [form, xpos]:
+                # Most of the time the UD wordform and XPOS will match
+                # what is stored in the .sst file.
+                # Exceptions: ASCII normalization and tag fixes.
+                print(data["words"][offset0], [form, xpos], file=sys.stderr)
+            # TODO: if data["lemmas"]: assert data["lemmas"][offset0] == lemma
+            smwe = smweGroup.get(tokNum, '_')
+            lexlemma = lexLemmas.get(tokNum, '_')
+            wmwe = wmweGroup.get(tokNum, '_')
+            wlemma = wLemmas.get(tokNum, '_')
+            #lexcat = data["lexcat"].get(str(tokNum), '_') # TODO: is lexcat ever manually specified, or always inferred from UD + MWE annotations?
+
+
+            #wcat = data["wcat"].get(str(tokNum), '_')  # TODO
+            wcat = '_'
+            ss = data["labels"].get(str(tokNum), '__')[1]
+            if ss=='NATURAL OBJECT':
+                ss = 'NATURALOBJECT'
+
+            lexcat = compute_lexcat(tokNum, smwe, smweGroupToks.get(tokNum), ss, lexlemma, poses, rels)
+
+            PSS = ('P','PP','INF.P','POSS','PRON.POSS')
+            if '|' in ss:
+                assert lexcat in PSS,lexcat
+                ss1, ss2 = ss.split('|')
+                ss1 = 'p.'+ss1
+                ss2 = 'p.'+ss2
+            elif lexcat in PSS:
+                ss1 = ss2 = 'p.'+ss
+            else:   # noun, verb expressions only have 1 supersense
+                if ss!='_':
+                    if ss.isalpha() and ss.isupper():
+                        ss = 'n.'+ss
+                    elif ss.isalpha() and ss.islower():
+                        ss = 'v.'+ss
+                    else:
+                        try:
+                            assert ss[0]=='`' or ss=='??',ss
+                        except AssertionError as ex:
+                            print(ex, file=sys.stderr)
+                        if ss[0]=='`' and ss!='`$':
+                            ss = '_'    # most backtick labels redundant with lexcat
+                ss1 = ss
+                ss2 = '_'
+            fulllextag = '_'    # TODO
+            newFields = [smwe, lexcat, lexlemma, ss1, ss2, wmwe, wcat, wlemma, fulllextag]
+            print(ln + '\t' + '\t'.join(newFields))
+        else:   # an ellipsis node, e.g. 10.1
+            print(ln + '\t' + '\t'.join('_'*N_NEW_COLS))
+    print()
