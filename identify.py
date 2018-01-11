a2da6913fc55d0040f438d9ff67413d147967d5a
Jakob Prange <jp1724@georgetown.edu>
2018-01-10 20:30:12 -0500

    revised heuristics in identify.py; v1.1 [all ADPs that are not RPs; all PRP$, WP$, POS; tokens that are both ADV and RB and whose lemma is in list; TOs whose head's role is advcl; MWEs according to list of MWEs that occur at least twice in training data]

--- identify.py
+++ identify.py
@@ -2,7 +2,12 @@ import sys
 import argparse
 import re
 
-PREPS_MASTER = {"2", "4", "a", "abaft", "aboard", "about", "above", "abreast", "abroad", "absent", "across",
+from collections import defaultdict
+
+
+from helpers import *
+
+PREPS_MASTER = {"a", "abaft", "aboard", "about", "above", "abreast", "abroad", "absent", "across",
                     "adrift", "afore", "aft", "after", "afterward", "afterwards", "against", "agin", "ago",
                     "aground", "ahead", "aloft", "along", "alongside", "amid", "amidst", "among", "amongst",
                     "an", "anent", "anti", "apart", "apropos", "apud", "around", "as", "ashore", "aside",
@@ -72,47 +77,22 @@ PREP_SPECIAL_MW_BEGINNERS = ["a", "according", "all", "bare", "because", "but", 
 
 
 
+
+
 TAGS_PTB = ["IN", "RB", "RP", "PRP$", "WP$", "POS", "TO"]
 TAGS_UD = ["ADP", "SCONJ", "ADV", "PRON", "PART"]
 
 
-
-class Token:
-    def __init__(self, string):
-        self.fields = string.split("\t")
-        self.offset, \
-        self.word, \
-        self.lemma, \
-        self.pos, \
-        self.xpos, \
-        self.morph, \
-        self.head, \
-        self.deprel = self.fields[:8]
-        self.orig = string
-
-class Sentence:
-    def __init__(self, tokens, meta):
-        self.tokens = tokens
-        self.meta = meta
-        
-def sentences(filename):
-    tokens, meta = [], []
-    with open(filename) as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                yield Sentence(tokens, meta)
-                tokens = []
-                meta = []
-            elif line.startswith("#"):
-                meta.append(line)
-            else:
-                tokens.append(Token(line))
-                
-    if tokens:
-        yield Sentence(tokens, meta)
-
-
+def print_target(token, sentence, index, checkmark, context):
+    for cont in range(context, 0, -1):
+        if index-cont >=0:
+            print("-{}".format(sentence.tokens[index-cont].orig))
+    print((":" if context else "")+"{}\t{}".format(token.orig, checkmark))
+    for cont in range(1, context+1):
+        if index+cont < len(sentence.tokens):
+            print("-{}".format(sentence.tokens[index+cont].orig))
+    if context:
+        print()
 
 
 def main(args):
@@ -122,72 +102,106 @@ def main(args):
     mwe = args.mwe
     evl = args.eval
 
-    tp, fp, fn, tn = 0, 0, 0, 0
-
-    
-    if tagset == "PTB":
-        tags = TAGS_PTB
-        field = 4
+    mwe_list = set()
+    if args.mwe_list:
+        with open(args.mwe_list) as f:
+            for line in f:
+                mwe_list.add(line.strip().split("\t")[0].strip())
     else:
-        tags = TAGS_UD
-        field = 3
-    tags.append("_MWE_")
+        mwe_list = PREPS_MASTER        
+
+    tp, fp, fn, tn = 0, 0, 0, 0
+    
+    lemma_pos_counts = {}
+    for sent in sentences(infile):
+        for token in sent.tokens:
+            if token.lemma not in lemma_pos_counts:
+                lemma_pos_counts[token.lemma] = defaultdict(int)
+            lemma_pos_counts[token.lemma][token.ud_pos] += 1
+            lemma_pos_counts[token.lemma][token.ptb_pos] += 1
         
-    max_mwe_length = max(len(w.split()) for w in PREPS_MASTER)
-    mw_beginners = set([w.split()[0] for w in PREPS_MASTER if len(w.split()) >= 2]).union(set(PREP_SPECIAL_MW_BEGINNERS))
+    max_mwe_length = max(len(w.split()) for w in mwe_list)
+    mw_beginners = set([w.split()[0] for w in mwe_list if len(w.split()) >= 2]).union(set(PREP_SPECIAL_MW_BEGINNERS))
 
     for sent in sentences(infile):
-        if not evl:
+        if not (evl or args.tp or args.fp or args.fn or args.tn):
             for metaline in sent.meta:
                 print(metaline)
 
         length = len(sent.tokens)
-        for i, token in enumerate(sent.tokens):
-            pos = token.fields[field]
-            if evl:
+        i = 0
+        k = 0
+        while i < length:
+            token = sent.tokens[i]
+            if (evl or args.tp or args.fp or args.fn or args.tn):
                 try:
                     xlemma = token.fields[12]
                     lexcat = token.fields[13]
                 except IndexError as e:
-                    print("NOTE: the --eval option works ONLY with full .conllulex format", file=sys.stderr)
+                    print("NOTE: the --eval, --tp, --fp, --fn, --tn options works ONLY with full .conllulex format", file=sys.stderr)
                     sys.exit(1)
 
             t = False
-            if evl and re.match("^(p|\?)", lexcat):
+            if (evl or args.tp or args.fp or args.fn or args.tn) and re.match("^(p|\?)", lexcat):
                 t = True
 
             lemma = token.lemma
-            checkmark = "*"
-            if mwe and token.lemma in mw_beginners: # find the longest possible mwe
-                for j in range(min(length, i+max_mwe_length)-1, i+1, -1):
-                    ngram = [t for t in sent.tokens[i:j]]
-                    ngram_lemma = " ".join([t.lemma for t in ngram])
-                    if ngram_lemma in PREPS_MASTER:
-                        lemma = ngram_lemma
-                        checkmark = "**"
-                        break
+            checkmark = ""
+            if i>=k:
+                if mwe and token.lemma in mw_beginners: # find the longest possible mwe
+                    for j in range(min(length, i+max_mwe_length)-1, i+1, -1):
+                        ngram = [t for t in sent.tokens[i:j]]
+                        ngram_lemma = " ".join([t.lemma for t in ngram])
+                        if ngram_lemma in mwe_list:
+                            lemma = ngram_lemma
+                            checkmark = lemma + "_**"
+                            k = j
+                            break
 
-            if pos in tags and ((pos not in ["IN", "RB", "ADV"]) or (lemma in PREPS_MASTER)):
-                if evl:
-                    if t:
-                        tp += 1
-                    else:
-                        fp += 1
+                if not checkmark:
+                    if token.ud_pos == "ADP" and token.ptb_pos != "RP" and token.lemma in PREPS_MASTER:
+                        checkmark = "ADP_*"
+                    elif token.ptb_pos in {"PRP$", "WP$", "POS"}:
+                        checkmark = "PRP$_WP$_POS_*"
+                    elif token.ud_pos == "SCONJ" and token.lemma in PREPS_MASTER: # and lemma_pos_counts[token.lemma]["ADP"] >= 20:
+                        checkmark = "SCONJ_*"
+                    elif token.ud_pos == "ADV" and token.ptb_pos == "RB" and token.lemma in PREPS_MASTER:
+                        checkmark = "ADV_RB_*"
+                    elif token.ptb_pos == "TO":
+                        head = sent.tokens[int(token.head)-1]
+                        matrix = sent.tokens[int(head.head)-1]
+                        head_has_dir_obj = any(t.head == head.head and t.deprel == "obj" for t in sent.tokens)
+                        if head.deprel in {"advcl"}:
+                            checkmark = "TO_*"
+            if checkmark:
+                if t:
+                    if args.tp and not evl:
+                        print_target(token, sent, i, checkmark, args.context)
+                    tp += 1
                 else:
-                    print("{}\t{}\t{}".format(token.orig, lemma, checkmark))
+                    if args.fp and not evl:
+                        print_target(token, sent, i, checkmark, args.context)
+                    fp += 1
             else:
-                if evl:
-                    if t:
-                        fn += 1
-                    else:
-                        tn += 1
+                if t:
+                    if args.fn and not evl:
+                        print_target(token, sent, i, checkmark, args.context)
+                    fn += 1
                 else:
-                    print(token.orig)
+                    if args.tn and not evl:
+                        print_target(token, sent, i, checkmark, args.context)
+                    tn += 1
+            if not (evl or args.tp or args.fp or args.fn or args.tn):
+                print("{}\t{}".format(token.orig, checkmark))
+
+            i += 1
+        if not (evl or args.tp or args.fp or args.fn or args.tn):
+            print()
 
     if evl:
-        print("\ttrue+\ttrue-")
-        print("pred+\t{}\t{}\t{}".format(tp, fp, tp+fp))
-        print("pred-\t{}\t{}\t{}".format(fn, tn, fn+tn))
+        print("\tgold+\tgold-")
+        print("auto+\t{}\t{}\t{}".format(tp, fp, tp+fp))
+        print("auto-\t{}\t{}\t{}".format(fn, tn, fn+tn))
         print("\t{}\t{}\t{}".format(tp+fn, fp+tn, tp+fp+tn+fn))
         p = tp/(tp+fp)
         r = tp/(tp+fn)
@@ -200,7 +214,14 @@ if __name__ == "__main__":
     parser.add_argument('file', type=str, help='path to the .conllulex file')
     parser.add_argument('-t', '--tagset', type=str, help='one of {UD, PTB}', default="PTB")
     parser.add_argument('-m', '--mwe', action='store_true', help='also look for mwes')
+    parser.add_argument('-l', '--mwe-list', type=str, help='read lexical list of MWEs from file MWE_LIST')
     parser.add_argument('-e', '--eval', action='store_true', help='output evaluation instead of annotated lines; works ONLY with full .conllulex format')
+    parser.add_argument('-a', '--tp', action='store_true', help='true positives')
+    parser.add_argument('-b', '--fp', action='store_true', help='false positives')
+    parser.add_argument('-c', '--fn', action='store_true', help='false negatives')
+    parser.add_argument('-d', '--tn', action='store_true', help='true negatives')
+    parser.add_argument('-n', '--context', type=int, default=0, help='number of context lines to print before and after target (only has effect when used with --tp, --fp, --fn, or --tn)')
+    
 #    parser.add_argument('-v', '--verbose', action='store_true', help='')
 
     args = parser.parse_args()
