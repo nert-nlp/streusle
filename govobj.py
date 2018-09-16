cf017bef06b566cac528ae4a73bc125f1251747b
Nathan Schneider <nathan.schneider@georgetown.edu>
2018-09-16 01:18:52 -0400

    govobj.py: improve heuristics (#35)

--- govobj.py
+++ govobj.py
@@ -68,6 +68,35 @@ import sys, json
 from collections import Counter
 from itertools import chain
 
+def enhance(sent):
+    """
+    For tokens where deprel is "conj", use Enhanced Dependencies to get a propagated head.
+    """
+    for tok in sent['toks']:
+        if tok['deprel']=='conj':
+            assert tok['edeps']
+            edeps = [ed for ed in tok['edeps'].split('|') if ':conj' not in ed]
+            tok['bhead'] = tok['head']
+            tok['bdeprel'] = tok['deprel']
+            if not edeps:   # essentially a root
+                tok['head'] = 0
+                tok['deprel'] = 'root'
+            else:   # arbitrarily choose the first of the enhanced deprels that are not conj
+                ed = edeps[0].split(':')
+                tok['head'] = int(ed[0].split('.')[0])  # if a copy node, e.g. "7.1", set head to 7
+                tok['deprel'] = ed[1]
+            
+def deenhance(sent):
+    """
+    Reinstate Basic Dependencies deprels.
+    """
+    for tok in sent['toks']:
+        if 'bhead' in tok:
+            tok['head'] = tok['bhead']
+            tok['deprel'] = tok['bdeprel']
+            del tok['bhead']
+            del tok['bdeprel']
+
 def findsubj(tok, sent):
     t = tok['#']
     for tok2 in sent['toks']:
@@ -79,6 +108,13 @@ def findcop(tok, sent):
     t = tok['#']
     for tok2 in sent['toks']:
         if tok2['head']==t and tok2['deprel'] in {'cop'}:
+            return tok2
+    return None
+
+def findobl(tok, sent):
+    t = tok['#']
+    for tok2 in sent['toks']:
+        if tok2['head']==t and tok2['deprel'] in {'obl:npmod'}:
             return tok2
     return None
 
@@ -107,6 +143,55 @@ def findgovobj(pexpr, sent):
         pptop = sent['toks'][tok1['head']-1] if tok1['head']>0 else None
         if otok is None:
             otok = pptop
+        #if pptop['deprel']=='conj' and not findcop(pptop, sent): # non-copular coordinated PP (coordination between 2 PPs, or coordinated infinitive phrase or possessive NP)
+        #     pptop = sent['toks'][pptop['head']-1]  # go up another level
+        #     #print('\n', 'gov=', sent['toks'][pptop['head']-1]['word'], plemma, 'obj=', otok['word'], '    ', sent['text'], file=sys.stderr)
+        if tok1['lemma']=='as' and sent['toks'][pptop['head']-1]['lemma']=='as': # 2nd AS in as-as construction
+            pptop = sent['toks'][pptop['head']-1]   # essentially treat the object of the first AS as the governor of the 2nd AS. "as tall AS a horse": gov = tall, obj = horse
+        elif prel=='case' and sent['toks'][pptop['head']-1]['upos']=='ADV' and pptop['deprel'] in ('obl', 'nmod') \
+            and sent['toks'][pptop['head']-1]['lemma'] in ('back', 'down', 'out', 'over', 'away', 'home') \
+            and not (sent['toks'][pptop['head']-1]['smwe'] and sent['toks'][pptop['head']-1]['smwe'][1]>1) \
+            and not findcop(sent['toks'][pptop['head']-1], sent):
+            # correct for weird (and inconsistent) UD analysis where intransitive adposition (ADV) has a PP complement: 
+            # "got back FROM france", "made back IN the 60s", "drive 10 minutes more down TO Stevens_Creek", "over BY 16th and 15th"
+            pptop = sent['toks'][pptop['head']-1]
+            assert not findcop(pptop, sent),(plemma,pptop)
+    elif plemma in ('ago', 'hence'):    # we consider these postpositions, UD considers them adverbs with extent modifiers (obl:npmod)
+        pptop = tok1
+        otok = findobl(tok1, sent)
+    elif prel=='advmod' and tok1['head'] > t1:
+        pptop = sent['toks'][tok1['head']-1]
+        if tok1['lemma']=='as': # first AS in as-as construction, as_soon_as, as_long_as
+            otok = pptop    # "tall" in "as tall as a horse"
+            #print(sent['mwe'], otok, file=sys.stderr)
+        elif tok1['head'] in pexpr['toknums']:    # idiomatic PPs of the form advmod(w2,w1): just_about, out_there, up_front, at_first
+            if sent['toks'][tok1['head']-1]['head'] > tok1['head'] and sent['toks'][tok1['head']-1]['deprel']=='advmod':    # just_about
+                otok = pptop    # "everything" in "just about everything"
+            # else out_there, up_front, at_first: no obj
+        elif pptop['upos']=='ADV':  # "back home", "down there" (also "over and over")
+            pass    # treat "back", "down" as intransitive particles (otok = None), use governor of "home"/"there" as the governor of the preposition
+        elif len(pexpr['toknums'])==1 and sent['toks'][t1+1-1]['upos']=='ADP' and sent['toks'][t1+1-1]['head']==tok1['head']:   # "back between" X and Y, "bank in June", "back to me"
+            # treat "back" as intransitive particle (otok = None)
+            pass
+        elif len(pexpr['toknums'])==2 and sent['toks'][pexpr['toknums'][1]-1]['head']==t1 and sent['toks'][pexpr['toknums'][1]-1]['deprel']=='fixed':
+            if plemma=='at least':  # "at_least pretend to be helpful", fixed(at, least): no object
+                pptop = tok1
+            else:   # "all_of 10 minutes", "less_than an hour" (Approximators): treat as transitive P with no governor
+                otok = pptop
+                pptop = tok1
+        elif pexpr['ss']=='p.Approximator': # single-word Approximators: "about", "around", "like", "over". 
+            # In UD treated as advmod of the measured expression. We treat as transitive P with no governor.
+            otok = pptop
+            pptop = tok1
+            
+        elif len(pexpr['toknums'])>1:
+            pass
+        else:   # adverb fronted before verb: "We have since moved", "I've never before felt...", "never before has...", "off we went"
+            pptop = tok1    # intransitive
+    
+        # two kinds of at_least: 
+        #   "at least 10 more minutes" - right-headed: case(least, at) 
+        #   "at least pretend to be helpful" - left-headed: fixed(at, least)
     else:
         pptop = tok1
         #if otok is None, no (local) object/complement
@@ -139,6 +224,10 @@ def findgovobj(pexpr, sent):
     if not config:
         config = 'default'
 
+    if gtok==otok!=None:
+        # Approximators ("about 4 bucks"): remove the governor
+        gtok = None
+
     pexpr['heuristic_relation'] = {
              'gov': gtok['#']     if gtok else None,
         'govlemma': gtok['lemma'] if gtok else None,
@@ -146,6 +235,20 @@ def findgovobj(pexpr, sent):
         'objlemma': otok['lemma'] if otok else None,
           'config': config
     }
+    
+    if gtok:
+        glexe = sent['swes'].get(str(sent['toks'][gtok['#']-1]['#']))
+        #if not glexe:
+        #    glexe = sent['smwes'][str(sent['toks'][gtok['#']-1]['smwe'][0])]
+        #assert glexe
+        if glexe and pexpr['lexcat'] not in ('POSS', 'PRON.POSS'):    # "on_ our _way", "on_ my _own", etc. are legitimate cases of a SNACS expression governed by another SNACS expression
+            if glexe['ss'] and glexe['ss'].startswith('p.'):
+                #pass
+                print(pexpr['lexcat'], plemma, '   ', sent['mwe'], file=sys.stderr) # "back from", "back to", ...
+                #print(pexpr, sent['mwe'], file=sys.stderr)
+                
+            # legitimate: ABOUT a month ago: gov = "ago". similar to "from BEHIND the couch"
+            # legitimate copular intransitive P + PP: I was in two weeks AGO: gov = "in"; "they were out FOR the day": gov = "out"
 
     #print(sent['mwe'], (gtok['word'], plemma, otok['word']), config)
 
@@ -153,8 +256,10 @@ with open(sys.argv[1], encoding='utf-8') as inF:
     data = json.load(inF)
 
 for sent in data:
+    enhance(sent)
     for lexe in chain(sent['swes'].values(), sent['smwes'].values()):
         if lexe['lexcat'] in {'P','PP','INF.P','POSS','PRON.POSS'}:
             gov = findgovobj(lexe, sent)
+    deenhance(sent)
 
 print(json.dumps(data, indent=1))
