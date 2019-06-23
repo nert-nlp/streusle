e4f31f01e49e1a071dee97968288a3ddcc8a951f
Nathan Schneider <nathan.schneider@georgetown.edu>
2019-06-23 16:09:32 -0400

    release.sh: produce .json and .govobj.json files for splits; in main STATS.md, link to ones for splits

--- release.sh
+++ release.sh
@@ -16,14 +16,28 @@ python split.py streusle.conllulex ud_test_sent_ids.txt > test/streusle.ud_test.
 
 cd train
 ../stats.sh streusle.ud_train.conllulex
+
+../conllulex2json.py streusle.ud_train.conllulex > streusle.ud_train.json
+
+../govobj.py streusle.ud_train.conllulex > streusle.ud_train.govobj.json
 cd -
 
 cd dev
 ../stats.sh streusle.ud_dev.conllulex
+
+../conllulex2json.py streusle.ud_dev.conllulex > streusle.ud_dev.json
+
+../govobj.py streusle.ud_dev.conllulex > streusle.ud_dev.govobj.json
 cd -
 
 cd test
 ../stats.sh streusle.ud_test.conllulex
+
+../conllulex2json.py streusle.ud_test.conllulex > streusle.ud_test.json
+
+../govobj.py streusle.ud_test.conllulex > streusle.ud_test.govobj.json
 cd -
+
+echo "* [train](train/STATS.md), [dev](dev/STATS.md), [test](test/STATS.md)" >> STATS.md
 
 echo "Done with release.sh"
