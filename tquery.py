84204849844fab9c133720217b48f014c6e99719
Nathan Schneider <nathan.schneider@georgetown.edu>
2019-09-08 15:34:19 -0400

    tquery.py refactoring

--- tquery.py
+++ tquery.py
@@ -33,7 +33,7 @@ op: if filtering on the field, one of: = (regex partial match), == (regex full m
 
 pattern: if filtering on the field, regex to match against the field's value; case-insensitive unless -I is specified
 
-Note that for prepositions/possessives, lextag contains the full supersense labeling in "role|function" notation. 
+Note that for prepositions/possessives, lextag contains the full supersense labeling in "role|function" notation.
 So lextag can be used to search for a supersense without specifying whether it occurs as role or function.
 
 Properties of the governor and object cannot be referenced unless the govobj.py script has been run to add the information to the JSON.
@@ -44,7 +44,7 @@ An example use of this script to query preposition tokens:
     ./govobj.py streusle.json > streusle.go.json
     ./tquery.py streusle.go.json lc==PP? +ll +r +f +config!=subordinating +g +g.upos +o=. +o.upos > streusle.ptoks.tsv
 
-Outputs the tokens annotated as P or (idiomatic) PP along with their role/function supersenses, 
+Outputs the tokens annotated as P or (idiomatic) PP along with their role/function supersenses,
 syntactic configuration (default, predicative, or stranded), and lemmas and POSes of the governor and (non-empty) object.
 
 @author: Nathan Schneider (@nschneid)
@@ -56,8 +56,8 @@ import shlex, subprocess
 from itertools import chain
 
 TKN_LEVEL_FIELDS = {'w': 'word', 'word': 'word', 'l': 'lemma', 'lemma': 'lemma',
-                   'upos': 'upos', 'xpos': 'xpos', 'feats': 'feats', 
-                   'head': 'head', 'deprel': 'deprel', 'edeps': 'edeps', 
+                   'upos': 'upos', 'xpos': 'xpos', 'feats': 'feats',
+                   'head': 'head', 'deprel': 'deprel', 'edeps': 'edeps',
                    'misc': 'misc', 'smwe': 'smwe', 'wmwe': 'wmwe', 'lextag': 'lextag', 'lt': 'lextag'}
 LEX_LEVEL_FIELDS = {'lexcat': 'lexcat', 'lc': 'lexcat', 'lexlemma': 'lexlemma', 'll': 'lexlemma',
                    'r': 'ss', 'ss': 'ss', 'f': 'ss2', 'ss2': 'ss2'}
@@ -65,187 +65,197 @@ GOVOBJ_FIELDS = {'g': 'govlemma', 'govlemma': 'govlemma', 'o': 'objlemma', 'objl
 ALL_FIELDS = dict(**TKN_LEVEL_FIELDS, **LEX_LEVEL_FIELDS, **GOVOBJ_FIELDS)
 RE_FLAGS = re.IGNORECASE   # case-insensitive by default
 
-printHeader = True
-printSentId = True
-printTokOffset = True
+def tquery(jsonPath, select_fields):
+
+    with open(inFP, encoding='utf-8') as inF:
+        data = json.load(inF)
 
 
-args = sys.argv[1:]
-assert len(args)>=2
 
-while args[0].startswith('-'):
-    flag = args.pop(0)
-    if flag=='-H':  # no header info
-        printHeader = False
-    elif flag=='-I': # case-sensitive
-        RE_FLAGS = 0
-    elif flag=='-S':    # no sentence IDs
-        printSentId = False
-    elif flag=='-T':    # no token offsets
-        printTokOffset = False
-    else:
-        raise ValueError(f'Invalid flag: {flag}')
-
-inFP = args.pop(0)
-constraints = tknconstraints, lexconstraints, govobjconstraints = [], [], []
-prints = [] # fields whose values are to be printed
-
-if printSentId:
-    prints.append('_sentid')
-if printTokOffset:
-    prints.append('_tokoffset')
-
-for arg in args:
-    printme = False
-    if arg.startswith('+'):
-        printme = True
-        arg = arg[1:]
-        
-    if '=' in arg:
-        fld, pattern = arg.split('=', 1)
-    
-        if fld.endswith('!'):
-            op = '!='
-            fld = fld[:-1]
-            if pattern.startswith('='):
-                op += '='
-                pattern = '^' + pattern[1:] + '$'
-            r = re.compile(pattern, RE_FLAGS)
-            matchX = (lambda r: lambda s: s is not None and r.search(s) is None)(r)
-        elif pattern.startswith('='):
-            op = '=='
-            pattern = pattern[1:]
-            r = re.compile('^'+pattern+'$', RE_FLAGS)
-            matchX = (lambda r: lambda s: s is not None and r.search(s) is not None)(r)
-        else:
-            op = '='
-            r = re.compile(pattern, RE_FLAGS)
-            matchX = (lambda r: lambda s: s is not None and r.search(s) is not None)(r)
-    
-        if '.' in fld:
-            prefix, fld = fld.split('.',1)
-            prefix += '.'   # g. (governor) or o. (object)
-        else:
-            prefix = ''
-        fld = ALL_FIELDS[fld]
-        fld = prefix+fld
-        if fld in TKN_LEVEL_FIELDS:
-            tknconstraints.append((fld, matchX))
-        elif fld in LEX_LEVEL_FIELDS:
-            lexconstraints.append((fld, matchX))
-        else:
-            govobjconstraints.append((fld, matchX))
-    else:
-        assert printme
-        fld = arg
-        
-    if printme:
-        if '.' in fld:
-            prefix, fld = fld.split('.',1)
-            prefix += '.'   # g. (governor) or o. (object)
-        else:
-            prefix = ''
-        fld = ALL_FIELDS[fld]
-        fld = prefix+fld
-        if fld not in prints:
-            prints.append(fld)
-        # to the "constraints", add a dummy item indicate that the field should be looked up for printing
-        if fld in TKN_LEVEL_FIELDS:
-            tknconstraints.append((fld, None))
-        elif fld in LEX_LEVEL_FIELDS:
-            lexconstraints.append((fld, None))
-        else:
-            govobjconstraints.append((fld, None))
-
-
-with open(inFP, encoding='utf-8') as inF:
-    data = json.load(inF)
-
-if printHeader:
-    # for reproducibility, the git commit hash and the command line call to this script
-    commitHash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
-    sysCall = sys.argv[0] + " " + " ".join(map(shlex.quote, sys.argv[1:]))
-    print(f'# {commitHash} ~ {sysCall}')
-
-    # column headers
-    print('# ' + '\t'.join(prints), 'tokInContext', sep='\t')
-
-n = 0
-for sent in data:
-    for lexe in chain(sent["swes"].values(), sent["smwes"].values()):
-        fail = False
-        myprints = {k: None for k in prints}
-        # at the lexical expression level: lexcat, lexlemma, ss (role), ss2 (function), heuristic_relation["govlemma", "objlemma", "config"]
-        for fld, matchX in lexconstraints:
-            if matchX and not matchX(lexe[fld]):
-                fail = True
-                break
-            if matchX is None:
-                myprints[fld] = lexe[fld]
-        if govobjconstraints and not fail:
-            if "heuristic_relation" not in lexe:
-                fail = True
-            else:
-                govobj = lexe["heuristic_relation"]
-                for fld, matchX in govobjconstraints:
-                    if '.' in fld:
-                        assert fld.startswith('g.') or fld.startswith('o.')
-                        i = govobj["gov"] if fld.startswith('g.') else govobj["obj"]
-                        if i is None:
-                            if matchX:
-                                fail = True
-                                break
-                            else:
-                                go = {'': ''}
-                                f = ''
-                        else:
-                            go = sent["toks"][i-1]
-                            f = fld.split('.',1)[1]
-                    else:
-                        go = govobj
-                        f = fld
-                    
-                    if matchX and not matchX(go[f]):
-                        fail = True
-                        break
-                    if matchX is None:
-                        myprints[fld] = go[f]
-        if tknconstraints and not fail:
-            toks = [sent["toks"][i-1] for i in lexe["toknums"]]
-            for fld, matchX in tknconstraints:
-                if matchX and not any(matchX(tok[fld]) for tok in toks):
+    n = 0
+    for sent in data:
+        for lexe in chain(sent["swes"].values(), sent["smwes"].values()):
+            fail = False
+            myprints = {k: None for k in prints}
+            # at the lexical expression level: lexcat, lexlemma, ss (role), ss2 (function), heuristic_relation["govlemma", "objlemma", "config"]
+            for fld, matchX in lexconstraints:
+                if matchX and not matchX(lexe[fld]):
                     fail = True
                     break
                 if matchX is None:
-                    myprints[fld] = tuple(tok[fld] for tok in toks)
-        
-        if not fail:
-            myprints['_sentid'] = sent["sent_id"]
-
-            s = ''
-            inmatch = False
-            toknums = lexe["toknums"]
-            for tok in sent["toks"]:
-                if tok["#"] in toknums:
-                    if not inmatch:
-                        inmatch = True
-                        s += '>> '
+                    myprints[fld] = lexe[fld]
+            if govobjconstraints and not fail:
+                if "heuristic_relation" not in lexe:
+                    fail = True
                 else:
-                    if inmatch:
-                        inmatch = False
-                        s += '<< '
-                s += tok["word"] + ' '
-            if inmatch:
-                s += '<< '
-            
-            if 1 < len(toknums) == max(toknums)-min(toknums)+1:
-                myprints['_tokoffset'] = f'{min(toknums)}-{max(toknums)}'
+                    govobj = lexe["heuristic_relation"]
+                    for fld, matchX in govobjconstraints:
+                        if '.' in fld:
+                            assert fld.startswith('g.') or fld.startswith('o.')
+                            i = govobj["gov"] if fld.startswith('g.') else govobj["obj"]
+                            if i is None:
+                                if matchX:
+                                    fail = True
+                                    break
+                                else:
+                                    go = {'': ''}
+                                    f = ''
+                            else:
+                                go = sent["toks"][i-1]
+                                f = fld.split('.',1)[1]
+                        else:
+                            go = govobj
+                            f = fld
+
+                        if matchX and not matchX(go[f]):
+                            fail = True
+                            break
+                        if matchX is None:
+                            myprints[fld] = go[f]
+            if tknconstraints and not fail:
+                toks = [sent["toks"][i-1] for i in lexe["toknums"]]
+                for fld, matchX in tknconstraints:
+                    if matchX and not any(matchX(tok[fld]) for tok in toks):
+                        fail = True
+                        break
+                    if matchX is None:
+                        myprints[fld] = tuple(tok[fld] for tok in toks)
+
+            if not fail:
+                myprints['_sentid'] = sent["sent_id"]
+
+                s = ''
+                inmatch = False
+                toknums = lexe["toknums"]
+                for tok in sent["toks"]:
+                    if tok["#"] in toknums:
+                        if not inmatch:
+                            inmatch = True
+                            s += '>> '
+                    else:
+                        if inmatch:
+                            inmatch = False
+                            s += '<< '
+                    s += tok["word"] + ' '
+                if inmatch:
+                    s += '<< '
+
+                if 1 < len(toknums) == max(toknums)-min(toknums)+1:
+                    myprints['_tokoffset'] = f'{min(toknums)}-{max(toknums)}'
+                else:
+                    myprints['_tokoffset'] = ','.join(map(str,lexe["toknums"]))
+
+
+if __name__=='__main__':
+
+
+    printHeader = True
+    printSentId = True
+    printTokOffset = True
+
+
+    args = sys.argv[1:]
+    assert len(args)>=2
+
+    while args[0].startswith('-'):
+        flag = args.pop(0)
+        if flag=='-H':  # no header info
+            printHeader = False
+        elif flag=='-I': # case-sensitive
+            RE_FLAGS = 0
+        elif flag=='-S':    # no sentence IDs
+            printSentId = False
+        elif flag=='-T':    # no token offsets
+            printTokOffset = False
+        else:
+            raise ValueError(f'Invalid flag: {flag}')
+
+    inFP = args.pop(0)
+    constraints = tknconstraints, lexconstraints, govobjconstraints = [], [], []
+    prints = [] # fields whose values are to be printed
+
+    if printSentId:
+        prints.append('_sentid')
+    if printTokOffset:
+        prints.append('_tokoffset')
+
+    # parse the query language (fields and constraints)
+    for arg in args:
+        printme = False
+        if arg.startswith('+'):
+            printme = True
+            arg = arg[1:]
+
+        if '=' in arg:
+            fld, pattern = arg.split('=', 1)
+
+            if fld.endswith('!'):
+                op = '!='
+                fld = fld[:-1]
+                if pattern.startswith('='):
+                    op += '='
+                    pattern = '^' + pattern[1:] + '$'
+                r = re.compile(pattern, RE_FLAGS)
+                matchX = (lambda r: lambda s: s is not None and r.search(s) is None)(r)
+            elif pattern.startswith('='):
+                op = '=='
+                pattern = pattern[1:]
+                r = re.compile('^'+pattern+'$', RE_FLAGS)
+                matchX = (lambda r: lambda s: s is not None and r.search(s) is not None)(r)
             else:
-                myprints['_tokoffset'] = ','.join(map(str,lexe["toknums"]))
+                op = '='
+                r = re.compile(pattern, RE_FLAGS)
+                matchX = (lambda r: lambda s: s is not None and r.search(s) is not None)(r)
 
-            print(*[myprints[f] for f in prints],
-                  #lexe["ss"]+('|'+lexe["ss2"] if lexe["ss2"] and lexe["ss2"]!=lexe["ss"] else ''),     # TODO: make a field for this
-                  s, sep='\t')
-            n += 1
+            if '.' in fld:
+                prefix, fld = fld.split('.',1)
+                prefix += '.'   # g. (governor) or o. (object)
+            else:
+                prefix = ''
+            fld = ALL_FIELDS[fld]
+            fld = prefix+fld
+            if fld in TKN_LEVEL_FIELDS:
+                tknconstraints.append((fld, matchX))
+            elif fld in LEX_LEVEL_FIELDS:
+                lexconstraints.append((fld, matchX))
+            else:
+                govobjconstraints.append((fld, matchX))
+        else:
+            assert printme
+            fld = arg
 
-print(f'{n} match' + ('es' if n!=1 else ''), file=sys.stderr)
+        if printme:
+            if '.' in fld:
+                prefix, fld = fld.split('.',1)
+                prefix += '.'   # g. (governor) or o. (object)
+            else:
+                prefix = ''
+            fld = ALL_FIELDS[fld]
+            fld = prefix+fld
+            if fld not in prints:
+                prints.append(fld)
+            # to the "constraints", add a dummy item indicate that the field should be looked up for printing
+            if fld in TKN_LEVEL_FIELDS:
+                tknconstraints.append((fld, None))
+            elif fld in LEX_LEVEL_FIELDS:
+                lexconstraints.append((fld, None))
+            else:
+                govobjconstraints.append((fld, None))
+
+
+        if printHeader:
+            # for reproducibility, the git commit hash and the command line call to this script
+            commitHash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
+            sysCall = sys.argv[0] + " " + " ".join(map(shlex.quote, sys.argv[1:]))
+            print(f'# {commitHash} ~ {sysCall}')
+
+            # column headers
+            print('# ' + '\t'.join(prints), 'tokInContext', sep='\t')
+    # TODO: iterate over tquery() output
+
+                print(*[myprints[f] for f in prints],
+                      #lexe["ss"]+('|'+lexe["ss2"] if lexe["ss2"] and lexe["ss2"]!=lexe["ss"] else ''),     # TODO: make a field for this
+                      s, sep='\t')
+                n += 1
+
+    print(f'{n} match' + ('es' if n!=1 else ''), file=sys.stderr)
