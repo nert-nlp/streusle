9129fc18eaab2c4518300bcfacf1bc1d21ba6bc7
Nathan Schneider <nathan.schneider@georgetown.edu>
2019-10-18 00:43:32 -0400

    split.py should specify python3

--- releaseutil/release.sh
+++ releaseutil/release.sh
@@ -12,11 +12,11 @@ mkdir -p {train,dev,test}
 
 $RELUTILDIR/stats.sh streusle.conllulex
 
-python $RELUTILDIR/split.py streusle.conllulex ud_train_sent_ids.txt > train/streusle.ud_train.conllulex
+$RELUTILDIR/split.py streusle.conllulex ud_train_sent_ids.txt > train/streusle.ud_train.conllulex
 
-python $RELUTILDIR/split.py streusle.conllulex ud_dev_sent_ids.txt > dev/streusle.ud_dev.conllulex
+$RELUTILDIR/split.py streusle.conllulex ud_dev_sent_ids.txt > dev/streusle.ud_dev.conllulex
 
-python $RELUTILDIR/split.py streusle.conllulex ud_test_sent_ids.txt > test/streusle.ud_test.conllulex
+$RELUTILDIR/split.py streusle.conllulex ud_test_sent_ids.txt > test/streusle.ud_test.conllulex
 
 cd train
 ../$RELUTILDIR/stats.sh streusle.ud_train.conllulex
