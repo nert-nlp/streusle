daa5b2749e2a59963107d45114c56240774cb0d0
Luke Gessler <lukegessler@gmail.com>
2020-02-05 22:25:55 -0500

    add output dir arg

--- split_streusle_json.py
+++ split_streusle_json.py
@@ -12,7 +12,7 @@ def sent2doc(sent_id):
     return doc_id
 
 
-def process_json(json_file):
+def process_json(json_file, output_dir):
     # read json
     with open(json_file, "r", encoding="utf-8") as f:
         sentences = json.load(f)
@@ -21,6 +21,7 @@ def process_json(json_file):
     current_doc = []
     doc_count = 0
 
+    os.makedirs(output_dir, exist_ok=True)
     for i, sentence in enumerate(sentences):
         sent_id = sentence["sent_id"]
         doc_id = sent2doc(sent_id)
@@ -28,12 +29,12 @@ def process_json(json_file):
 
         if i == len(sentences) - 1 or sent2doc(sentences[i + 1]["sent_id"]) != doc_id:
             with open(
-                os.path.join(base_dir, doc_id + ".json"), "w", encoding="utf-8"
+                os.path.join(output_dir, doc_id + ".json"), "w", encoding="utf-8"
             ) as f:
                 json.dump(current_doc, f)
                 current_doc = []
                 doc_count += 1
-    print("Wrote " + str(doc_count) + " docs to " + base_dir)
+    print("Wrote " + str(doc_count) + " docs to " + output_dir)
 
 
 if __name__ == "__main__":
@@ -43,4 +44,5 @@ if __name__ == "__main__":
         )
     )
     argparser.add_argument("json_file", type=str)
+    argparser.add_argument("output_dir", type=str)
     process_json(**vars(argparser.parse_args()))
