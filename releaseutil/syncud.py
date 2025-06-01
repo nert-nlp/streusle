2dc5ac3bff1a219e54b2541912d92e37c6fc9b5b
Nathan Schneider <nathan.schneider@georgetown.edu>
2025-06-01 00:40:51 -0400

    update UD to v2.13

--- releaseutil/syncud.py
+++ releaseutil/syncud.py
@@ -82,7 +82,7 @@ for sent in sentences(CONLLULEX):
                 assert old_lexcat==tok.ud_pos or old_lexcat=='PRON' and old_lextag=='O-PRON' and tok.ud_pos=='NOUN',(old_lextag,tok.ud_pos,newudtok.ud_pos)
                 # 'other', 'none', 'one' in some cases had UPOS NOUN/lexcat PRON
 
-                newlexcat = {'ADP': 'P', 'NOUN': 'N', 'VERB': 'V'}.get(newudtok.ud_pos, newudtok.ud_pos)
+                newlexcat = {'ADP': 'P', 'NOUN': 'N', 'PROPN': 'N', 'VERB': 'V'}.get(newudtok.ud_pos, newudtok.ud_pos)
                 if newlexcat=='N' and newudtok.lemma in ('etc.','etc','one'):
                     newlexcat = 'PRON'
                 streusle[1] = newlexcat
